f02f8b72ef19d7c26354f1103081198f8e5d9ba8
filipeam <afilipem@gmail.com>
2020-02-17 21:59:11 -0600

    added: first modifications for the udpclient with a main loop to send and receive data

--- udpclient.py
+++ udpclient.py
@@ -4,48 +4,36 @@ import sys
 host = 'localhost'
 port = 1024
 
-# create socket
+# create dgram udp socket
 print('# Creating socket')
 try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 except socket.error:
     print('Failed to create socket')
     sys.exit()
 
-print('# Getting remote IP address') 
-try:
-    remote_ip = socket.gethostbyname( host )
-except socket.gaierror:
-    print('Hostname could not be resolved. Exiting')
-    sys.exit()
+# main loop 
+while(1) :
+    msg = raw_input('Enter message to send : ')
 
-# Connect to remote server
-print('# Connecting to server, ' + host + ' (' + remote_ip + ')')
-s.connect((remote_ip , port))
+    print('# Sending data to server')
+    try :
+		#Send the whole string
+		s.sendto(msg, (host, port))
 
-# Get text input from the user
-sentence = input('Input lowercase sentence:')
+        # Receive data
+        print('# Receiving data from server')
+        full_reply = ''
+        while True:
+            reply = s.recvfrom(8)
+            if len(reply) == 0:
+                break
+            full_reply += reply.decode()
 
+        print (full_reply)
 
-# Send data to remote server
-print('# Sending data to server')
-request = (sentence.encode())
+        print('# Finished!')
 
-try:
-    s.sendall(request)
-except socket.error:
-    print ('Send failed')
-    sys.exit()
-
-# Receive data
-print('# Receiving data from server')
-full_reply = ''
-while True:
-    reply = s.recv(8)
-    if len(reply) == 0:
-        break
-    full_reply += reply.decode()
-
-print (full_reply)
-
-print('# Finished!')
+    except socket.error, msg:
+		print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+		sys.exit()
